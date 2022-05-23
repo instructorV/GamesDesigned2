3950ab7cd05873865d70feb96f4b1ee4bca7c4c3
instructorV <98046352+instructorV@users.noreply.github.com>
2022-05-23 10:30:48 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,7 +10,7 @@ function preload(){
   karthikImage = loadImage("karthikGame.png")
   dharshithImage = loadImage("dharshithGame.png")
   gurucharanImage = loadImage("guruCharanGame.png")
-  gnanasreeImage = loadImage("gnanasreeGame.png")
+  gnanasreeImage = loadImage("gnanasreegame.png")
   jeevajenireethImage = loadImage("jeevajenireethGame.png")
   vyshnaviImage = loadImage("vyshnaviGame.png")
   sherlyImage = loadImage("sherlyGame.png")
